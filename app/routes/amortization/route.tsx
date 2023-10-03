90d05c52028ba5a1ee4858b799a0dbeebc9695dd
juandari <juandaari70@gmail.com>
2023-10-03 20:01:05 +0700

    feat: set validation

--- app/routes/amortization/route.tsx
+++ app/routes/amortization/route.tsx
@@ -12,13 +12,21 @@ import {
 } from "~/components/ui/table";
 import PageContainer from "~/components/page-container";
 import { formatCurrency } from "~/lib/numbers/format-currency";
-import { Card, CardContent, CardFooter } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import { Form } from "@remix-run/react";
 import { Label } from "~/components/ui/label";
 import NumericInput from "~/components/numeric-input";
 import { ButtonAnimate } from "~/components/button-animate";
 
 const MONTHS_IN_A_YEAR = 12;
+const MAX_TENURES = 36;
 
 export default function Amortization() {
   const [price, setPrice] = useState("");
@@ -27,6 +35,8 @@ export default function Amortization() {
   const [duration, setDuration] = useState("");
   const [fixDuration, setFixDuration] = useState("");
   const [floatingInterestRate, setFloatingInterestRate] = useState("13");
+  const [isMaxDuration, setIsMaxDuration] = useState(false);
+  const [floatingGap, setFloatingGap] = useState(0);
   const [amortizationData, setAmortizationData] = useState<
     ReturnType<typeof getAmortization>
   >([]);
@@ -42,6 +52,7 @@ export default function Amortization() {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setFloatingGap(Number(duration) - Number(fixDuration));
 
     const numPayments = Number(duration) * MONTHS_IN_A_YEAR;
     const interestRates: number[] = [];
@@ -67,9 +78,12 @@ export default function Amortization() {
 
   function resetForm() {
     setPrice("");
+    setDownPayment("");
     setInterestRate("");
     setDuration("");
+    setFixDuration("");
     setFloatingInterestRate("13");
+    setIsMaxDuration(false);
   }
 
   function handleResetForm(e: MouseEvent<HTMLButtonElement>) {
@@ -140,14 +154,40 @@ export default function Amortization() {
               <div className="mt-2">
                 <Label htmlFor="duration">Tenures (years)</Label>
                 <NumericInput
+                  className={`mt-2 ${
+                    isMaxDuration
+                      ? " focus-visible:ring-red-400 focus-visible:ring-offset-2"
+                      : ""
+                  }`}
                   id="duration"
                   value={duration}
                   onValueChange={(v) => setDuration(v.value)}
                   prefix=""
+                  isAllowed={(values) => {
+                    const { floatValue = 0 } = values;
+                    const belowLimit = floatValue < MAX_TENURES;
+
+                    if (!belowLimit) {
+                      setIsMaxDuration(true);
+                    } else {
+                      setIsMaxDuration(false);
+                    }
+
+                    return floatValue < MAX_TENURES;
+                  }}
                 />
+                <span
+                  className={`${
+                    isMaxDuration
+                      ? "opacity-100 transform translate-y-0"
+                      : "opacity-0 transform -translate-y-4"
+                  } text-xs text-red-500 transition-all duration-400 ease-in-out`}
+                >
+                  Maximum tenures is 35 years
+                </span>
               </div>
 
-              <CardFooter className="flex justify-end gap-2 p-0 mt-6">
+              <CardFooter className="flex justify-end gap-2 p-0 mt-2">
                 <ButtonAnimate variant="destructive" onClick={handleResetForm}>
                   Reset
                 </ButtonAnimate>
@@ -170,13 +210,36 @@ export default function Amortization() {
         <h2 className=" font-bold text-lg text-slate-800">
           Amortization Table
         </h2>
+        <Card className="mt-4">
+          <CardHeader className="pb-4">
+            <CardTitle>Monthly Payment</CardTitle>
+            <CardDescription>Before & After Floating Rate</CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-3">
+            <div>
+              <h3 className="text-md text-slate-700 font-semibold">Before</h3>
+              <span className="text-2xl text-green-400">
+                {formatCurrency(amortizationData[0]?.monthlyPayment)}
+              </span>
+            </div>
+            <div>
+              <h3 className="text-md text-slate-700 font-semibold">After</h3>
+              <span className="text-2xl text-red-400">
+                {formatCurrency(
+                  amortizationData[floatingGap * MONTHS_IN_A_YEAR]
+                    ?.monthlyPayment
+                )}
+              </span>
+            </div>
+          </CardContent>
+        </Card>
         <Card className={`mt-4`}>
           <CardContent>
             <Table className=" mt-10 rounded-lg">
               <TableHeader>
                 <TableRow>
                   <TableHead>Month</TableHead>
-                  <TableHead>Montly Payment</TableHead>
+                  <TableHead>Monthly Payment</TableHead>
                   <TableHead>Principal Payment</TableHead>
                   <TableHead>Interest Payment</TableHead>
                   <TableHead className="text-right">
--- app/routes/compound-interest/route.tsx
+++ app/routes/compound-interest/route.tsx
@@ -17,6 +17,8 @@ const frequencyMap = {
   monthly: 12,
 } satisfies Record<Frequency, number>;
 
+const MAX_DURATION = 101;
+
 export default function route() {
   const [initialInvestment, setInitialInvestment] = useState("");
   const [monthlyContribution, setMonthlyContribution] = useState("");
@@ -25,6 +27,7 @@ export default function route() {
   const [frequency, setFrequency] = useState<Frequency>("annually");
   const [finalBalance, setFinalBalance] = useState(0);
   const [isResultReady, setIsResultReady] = useState(false);
+  const [isMaxDuration, setIsMaxDuration] = useState(false);
   const yearsRef = useRef("");
 
   const isFormFilled =
@@ -53,6 +56,7 @@ export default function route() {
     setYears("");
     setInterest("");
     setFrequency("annually");
+    setIsMaxDuration(false);
   }
 
   function handleResetForm(e: MouseEvent<HTMLButtonElement>) {
@@ -80,7 +84,7 @@ export default function route() {
               />
             </div>
 
-            <div className="mt-2">
+            <div className="mt-4">
               <Label htmlFor="monthlyContribution">
                 Monthly Contribution (IDR)
               </Label>
@@ -91,14 +95,42 @@ export default function route() {
               />
             </div>
 
-            <div className="mt-2">
-              <Label htmlFor="years">Duration (years)</Label>
+            <div className="mt-4">
+              <Label className="mb-4" htmlFor="years">
+                Duration (years)
+              </Label>
               <NumericInput
+                className={`mt-2 ${
+                  isMaxDuration
+                    ? " focus-visible:ring-red-400 focus-visible:ring-offset-2"
+                    : ""
+                }`}
                 id="years"
                 value={years}
                 onValueChange={(v) => setYears(v.value)}
                 prefix=""
+                isAllowed={(values) => {
+                  const { floatValue = 0 } = values;
+                  const belowLimit = floatValue < MAX_DURATION;
+
+                  if (!belowLimit) {
+                    setIsMaxDuration(true);
+                  } else {
+                    setIsMaxDuration(false);
+                  }
+
+                  return floatValue < MAX_DURATION;
+                }}
               />
+              <span
+                className={`${
+                  isMaxDuration
+                    ? "opacity-100 transform translate-y-0"
+                    : "opacity-0 transform -translate-y-4"
+                } text-xs text-red-500 transition-all duration-400 ease-in-out`}
+              >
+                Maximum duration is 100 years
+              </span>
             </div>
 
             <div className="my-2">
@@ -119,7 +151,7 @@ export default function route() {
               <select
                 defaultValue={frequency}
                 onChange={handleChangeFrequency}
-                className="flex h-10 w-full mt-2 items-center justify-between rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300"
+                className="flex h-10 w-full mt-4 items-center justify-between rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300"
               >
                 <option value="annually">Annually</option>
                 <option value="monthly">Monthly</option>
@@ -139,7 +171,7 @@ export default function route() {
       </Card>
 
       <Card
-        className={`mt-2 transition-all duration-300 ease-out	 ${
+        className={`mt-4 transition-all duration-300 ease-out	 ${
           isResultReady
             ? "opacity-100 transform translate-y-0"
             : "opacity-0 transform -translate-y-4"
